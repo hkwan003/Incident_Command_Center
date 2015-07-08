a583ba492debff978a6b5f8f64fa55ce6bbf6c41
Calvin Kwan <hkwan003@ucr.edu>
2015-07-07 17:16:44 -0700

    got camera button up and almost back to where I left off

--- app/src/main/java/com/example/calvinkwan/incident_command_center/MainActivity.java
+++ app/src/main/java/com/example/calvinkwan/incident_command_center/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.calvinkwan.incident_command_center;
 
 import java.util.Locale;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -16,6 +19,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.DialogInterface.OnCancelListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,6 +29,31 @@ import com.parse.ParseUser;
 public class MainActivity extends AppCompatActivity
 {
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+    protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface dialog, int which)
+        {
+            switch(which)
+            {
+                case 0:     //take picture
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                    break;
+                case 1:     //take video
+                    break;
+                case 2:     //choose picture
+                    break;
+                case 3:     //choose video
+                    break;
+            }
+        }
+    };
     SectionsPagerAdapter mSectionsPagerAdapter;
 
 
@@ -81,6 +110,13 @@ public class MainActivity extends AppCompatActivity
         {
             ParseUser.logOut();
             navigateToLogin();
+        }
+        else if(id == R.id.media_actions)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setItems(R.array.camera_choices, mDialogListener);
+            AlertDialog dialog = builder.create();
+            dialog.show();
         }
         else if (id == R.id.action_edit_friends)
         {
