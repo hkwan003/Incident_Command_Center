e3eba195f2e0da8d21a70b84b9be5da23a15e147
Calvin Kwan <hkwan003@ucr.edu>
2015-07-13 13:19:26 -0700

    video and photo works mangs

--- app/src/main/java/com/example/calvinkwan/incident_command_center/InboxFragment.java
+++ app/src/main/java/com/example/calvinkwan/incident_command_center/InboxFragment.java
@@ -1,6 +1,7 @@
 package com.example.calvinkwan.incident_command_center;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -9,9 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -56,5 +60,31 @@ public class InboxFragment extends ListFragment
                 }
             }
         });
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        ParseFile  file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if(messageType.equals(ParseConstants.TYPE_IMAGE))
+        {
+            //view image
+            Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+            intent.setData(fileUri);
+            startActivity(intent);
+        }
+        else
+        {
+            //view video
+            Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
+            intent.setDataAndType(fileUri, "video/*");
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/calvinkwan/incident_command_center/MessageAdapter.java
+++ app/src/main/java/com/example/calvinkwan/incident_command_center/MessageAdapter.java
@@ -1,8 +1,15 @@
 package com.example.calvinkwan.incident_command_center;
 
+import android.view.ViewGroup;
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.v4.app.ListFragment;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
@@ -21,5 +28,44 @@ public class MessageAdapter extends ArrayAdapter<ParseObject>
         super(context, R.layout.message_item);
         mContext = context;
         mMessages = messges;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ViewHolder holder;
+
+
+        if (convertView == null)
+        {
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView)convertView.findViewById(R.id.messageIcon);
+            holder.nameLabel = (TextView)convertView.findViewById(R.id.senderLabel);
+            convertView.setTag(holder);
+        }
+
+        else
+        {
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+
+        ParseObject message = mMessages.get(position);
+        if(message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE))
+        {
+            holder.iconImageView.setImageResource(R.mipmap.ic_action_picture);
+        }
+        else
+        {
+            holder.iconImageView.setImageResource(R.mipmap.ic_action_play_over_video);
+        }
+        holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
+
+        return convertView;
+    }
+    private static class ViewHolder
+    {
+        ImageView iconImageView;
+        TextView nameLabel;
     }
 }
